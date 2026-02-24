eb8e12986553c0f0aabe1e314111e279a532a8fe
omeadow <omarmedeuov@gmail.com>
2026-02-24 20:30:59 +0500

    get main intranet page and serve its html with flask

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
 
+import scraper
+from scraper import html
+
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def send_request():
-	return "send request"
+	return html
---
+++ scraper.py
@@ -0,0 +1,4 @@
+import requests
+
+r = requests.get('https://my.kimep.kz')
+html = r.text
